70742b3fc574e7792a7e943a1ec09e0eeb1a8357
creepysta <sweetnanispregnantghost@yahoo.com>
2022-09-17 20:51:31 +0530

    refactored queries

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import "./App.css";
 import { client } from "./ApolloClient/client";
 import { ApolloProvider } from '@apollo/client';
-import ExchangeRatePage from './ExchangeRatePage';
+import { BooksPage } from './BooksPage';
 
-function App() {
+const App = () => {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <ExchangeRatePage />
+        <BooksPage />
       </div>
     </ApolloProvider>
   );
